a869c76c81a5527581f047adcb8922f92e472963
imran chaibi <chaibi1imran@gmail.com>
2022-09-29 11:05:11 +0100

    created menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
+    public static int s;
+    public static void menu() {
+        System.out.println("Welcome to admin");
+        System.out.println("----------------------------------------");
+        System.out.println("1. Manage students");
+        System.out.println("2. Manage teachers");
+        System.out.println("3. Manage subjects");
+        System.out.println("4. Manage classes");
+        System.out.println("5. Manage collages");
+        System.out.println("6. Exit");
+        System.out.println("----------------------------------------");
+        System.out.print("Enter your choice: ");
+        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            s = Integer.parseInt(bufferRead.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
-
+        do {
+            menu();
+            System.out.println(s);
+        }
+        while (s != 6);
     }
 }
--- src/college.java
+++ src/college.java
@@ -1,2 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
 public class college {
+    LinkedList collages = new LinkedList();
+    public void addCollage() {
+        System.out.println("Enter the name of the collage: ");
+        BufferedReader Name = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the Web site of the collage: ");
+        BufferedReader Website = new BufferedReader(new InputStreamReader(System.in));
+        // creating an array
+
+    }
 }
